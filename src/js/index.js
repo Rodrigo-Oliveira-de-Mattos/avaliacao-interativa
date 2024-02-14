24e725a26dbd0a32d04eb2f53c1480742ea52090
Rodrigo Oliveira <rodrigooliveiramattos2006@gmail.com>
2024-02-13 22:05:41 -0400

    JS adicionado

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,31 @@
 const inputAvaliacao = document.querySelectorAll('#li')
+const telaAvaliacao = document.getElementById('avaliacao')
+const telaAgradecimento = document.getElementById('agradecimento')
+let mensagemFinal = document.getElementById('mensagem-final')
+let valorFinal;
 
 inputAvaliacao.forEach((item) => {
     item.addEventListener('click', (evento) => {
-        console.log(evento.target);
+        // item.classList.add('clicado')
+        valor(evento.target.innerHTML);
     });
 });
 
+document.getElementById('btn-submit').addEventListener('click', () => {
+    if(valorFinal){
+        telaAvaliacao.classList.add('invisivel')
+        telaAgradecimento.classList.remove('invisivel')
+        console.log(mensagemFinal);
+        mensagemFinal.innerHTML = `You selected ${valorFinal} out of 5`
+        console.log(mensagemFinal);
+    }
+})
+
+function valor(valor){
+    return valorFinal = valor;
+}
+
+// You selected <span>4</span> out of 5
 
 // console.log(valor);
 
